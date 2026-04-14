f15d3d3a1a9b4823b080a1fb55bd3c82faf9b367
Mark Berthelemy <mark@berthelemy.net>
2026-04-14 13:12:08 +0200

    Move init variables to javascript

--- src/script.js
+++ src/script.js
@@ -1,2 +1,6 @@
 Config.passages.nobr = true;
 Config.history.controls = false;
+
+// This ensures the variables exist even if StoryInit hasn't run yet
+State.variables.integrity = 5;
+State.variables.budget = 5000;
